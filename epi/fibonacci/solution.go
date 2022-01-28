d0a37e2a190d9965b7ca8dcc67fa582d7f2f91c4
Riad Afridi Shibly <riadafridishibly@gmail.com>
2022-01-29 02:31:06 +0600

    add fibonacci

--- epi/fibonacci/solution.go
+++ epi/fibonacci/solution.go
@@ -1,6 +1,28 @@
 package fibonacci
 
+type fibCalculator struct {
+	cache map[int]int
+}
+
+func (fc *fibCalculator) calculate(n int) int {
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	if value, ok := fc.cache[n]; ok {
+		return value
+	}
+	value := fc.calculate(n-1) + fc.calculate(n-2)
+	fc.cache[n] = value
+
+	return value
+}
+
 func Fibonacci(n int) int {
-	// TODO - Add your code here
-	return 0
+	fc := fibCalculator{
+		cache: map[int]int{},
+	}
+	return fc.calculate(n)
 }
