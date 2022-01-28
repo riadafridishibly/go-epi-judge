091cc098d45f49216d5e74ed0cfebfd5e680d11e
Riad Afridi Shibly <riadafridishibly@gmail.com>
2022-01-28 14:45:00 +0600

    add even odd array solution

--- epi/even_odd_array/solution.go
+++ epi/even_odd_array/solution.go
@@ -1,5 +1,24 @@
 package even_odd_array
 
 func EvenOdd(a []int) {
-	// TODO - Add your code here
+	if len(a) < 2 {
+		return
+	}
+
+	evenIndex := 0
+	oddIndex := len(a) - 1
+
+	for evenIndex < oddIndex {
+		if a[evenIndex]%2 == 1 && a[oddIndex]%2 == 0 {
+			a[evenIndex], a[oddIndex] = a[oddIndex], a[evenIndex]
+		}
+
+		if a[evenIndex]%2 == 0 {
+			evenIndex++
+		}
+
+		if a[oddIndex]%2 == 1 {
+			oddIndex--
+		}
+	}
 }
